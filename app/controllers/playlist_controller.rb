01176973874d939b89ac64e1c161dae685e768ac
github username <github email address>
2020-08-06 14:19:35 -0500

    added flash messages

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -5,14 +5,17 @@ class PlaylistController < ApplicationController
   end
        
   post '/playlists/new' do
-     if @playlist = current_user.playlists.create(
+      @playlist = current_user.playlists.create(
        playlist_name: params[:playlist_name], 
        genre: params[:genre], 
        artist: params[:artist], 
        song: params[:song])
-       @playlist.save
-       erb :"/playlists/show"   
-     end     
+       if @playlist.save
+        erb :"/playlists/show"  
+       else
+        flash[:message] = "Spaces left blank!!"
+        redirect to :"/playlists/new"
+       end
   end
 
   get '/playlists/:id' do
@@ -52,8 +55,12 @@ class PlaylistController < ApplicationController
       genre: params[:genre], 
       artist: params[:artist], 
       song: params[:song])
+      if @playlist.save
       redirect to :"/playlists/#{@playlist.id}"
-  
+      else
+        flash[:message] = "Spaces left blank!!"
+        redirect to :"/playlists/#{@playlist.id}/edit"
+      end   
   end    
        
        
